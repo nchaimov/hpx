815423bbfc4b414227888d3ac36ea0c3a2430ee8
Nicholas Chaimov <nchaimov@uoregon.edu>
2014-04-23 12:04:13 -0700

    Add copyright header and Boost license to APEX files.

--- Apex/energyStat.h
+++ Apex/energyStat.h
@@ -1,3 +1,9 @@
+/*  Copyright (c) 2014 University of Oregon
+ *
+ *  Distributed under the Boost Software License, Version 1.0. (See accompanying
+ *  file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
+ */
+
 /*
  * starts daemon waittime in nanoseconds 0,000,000. currently ignored
  * and set to .1 sec -- code there but comment out since it was doing 
