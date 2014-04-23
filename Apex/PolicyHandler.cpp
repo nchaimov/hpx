3586a3e844908a4f640f75b038c2585b4968a9de
Nicholas Chaimov <nchaimov@cs.uoregon.edu>
2014-04-22 17:57:58 -0700

    Move APEX into the HPX source tree and build APEX with HPX if HPX_USE_APEX is set

---
+++ Apex/PolicyHandler.cpp
@@ -0,0 +1,67 @@
+#include "PolicyHandler.hpp"
+#include "ThreadInstance.hpp"
+
+#include <iostream>
+
+using namespace std;
+
+namespace apex {
+
+PolicyHandler::PolicyHandler (void) : Handler() { 
+  _init(); 
+}
+
+PolicyHandler::PolicyHandler (unsigned int period) : Handler(period) { 
+  _init(); 
+}
+
+void PolicyHandler::_handler(void) {
+  return;
+}
+
+void PolicyHandler::_init(void) {
+  _timer.async_wait(boost::bind(&PolicyHandler::_handler, this));
+  run();
+  return;
+}
+
+void PolicyHandler::onEvent(EventData* eventData) {
+  unsigned int tid = ThreadInstance::getID();
+  if (!_terminate) {
+    switch(eventData->eventType) {
+    case STARTUP: {
+    	cerr << tid << ": STARTUP" << endl;
+    	break;
+    }
+    case SHUTDOWN: {
+    	_terminate = true;
+    	cerr << tid << ": SHUTDOWN" << endl;
+    	break;
+    }
+    case NEW_NODE: {
+    	cerr << tid << ": NEW_NODE" << endl;
+    	break;
+    }
+    case NEW_THREAD: {
+    	cerr << tid << ": NEW_THREAD" << endl;
+    	break;
+    }
+    case START_EVENT: {
+    	cerr << tid << ": START_EVENT" << endl;
+    	break;
+    }
+    case STOP_EVENT: {
+    	cerr << tid << ": STOP_EVENT" << endl;
+    	break;
+    }
+    case SAMPLE_VALUE: {
+    	cerr << tid << ": SAMPLE_EVENT" << endl;
+    	break;
+    }
+
+    }
+  }
+  return;
+}
+
+}
